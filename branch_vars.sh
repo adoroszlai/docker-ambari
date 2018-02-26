e1f8039abf995a771bdb9d1d8719c8b87f78f637
Doroszlai, Attila <adoroszlai@apache.org>
2018-02-26 11:43:55 +0100

    Remove shell scripts

--- branch_vars.sh
+++
@@ -1,3 +0,0 @@
-: ${AMBARI_VERSION:="3.0.0.0-SNAPSHOT"}
-: ${BUILD:="latest"}
-: ${GIT_REF:="trunk"}
--- vars.sh
+++
@@ -1,4 +0,0 @@
-: ${FLAVORS:="centos7"}
-: ${GIT_REPO:="https://github.com/apache/ambari"}
-: ${HUB_REPO:="adoroszlai"}
-: ${MODULES:="ambari-agent ambari-server"}
